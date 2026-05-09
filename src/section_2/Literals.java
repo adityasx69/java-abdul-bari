5de4e9bd490104680b1142350480ec86d80f48dc
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-10 00:29:57 +0530

    added Literals

---
+++ src/section_2/Literals.java
@@ -0,0 +1,22 @@
+package section_2;
+
+public class Literals {
+    static void main() {
+        byte b1 = 10;
+        byte b2 = 0b1010;
+        byte b3 = 012;
+        byte b4 = 0XA;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println();
+
+        long l = 999_999_999L;
+        System.out.println(l);
+        System.out.println();
+        float f = 232_322.32f;
+        System.out.println(f);
+    }
+}
