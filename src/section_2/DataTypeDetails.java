ba18be7818080693d1a1b306fc79a8d9019559fa
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-10 00:58:21 +0530

    added DataTypeDetails

---
+++ src/section_2/DataTypeDetails.java
@@ -0,0 +1,10 @@
+package section_2;
+
+public class DataTypeDetails {
+    static void main() {
+        int x = 5;
+        int y = -5;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(y));
+    }
+}
