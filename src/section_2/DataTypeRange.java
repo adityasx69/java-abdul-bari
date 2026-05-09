79d49ea7085714e19eccf0a13f319a7cb38c089e
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-09 23:34:20 +0530

    added DataTypeRange

---
+++ src/section_2/DataTypeRange.java
@@ -0,0 +1,19 @@
+package section_2;
+
+public class DataTypeRange {
+    static void main() {
+        System.out.println("Integer Max Value " + Integer.MAX_VALUE);
+        System.out.println("Integer Min Value " + Integer.MIN_VALUE);
+        System.out.println("Integer Bytes " + Integer.BYTES);
+        System.out.println();
+
+        System.out.println("Double Max Value " + Double.MAX_VALUE);
+        System.out.println("Double Min Value " + Double.MIN_VALUE);
+        System.out.println("Double Bytes " + Double.BYTES);
+        System.out.println();
+
+        System.out.println("Character Max Value " + Character.MAX_VALUE);
+        System.out.println("Character Min Value " + Character.MIN_VALUE);
+        System.out.println("Character Bytes " + Character.BYTES);
+    }
+}
