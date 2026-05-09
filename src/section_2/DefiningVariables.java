3f12cdac6bf3f2cf830a8187c0a40247ea6e6454
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-10 00:03:09 +0530

    added  DefiningVariables

---
+++ src/section_2/DefiningVariables.java
@@ -0,0 +1,18 @@
+package section_2;
+
+public class DefiningVariables {
+    static void main() {
+        byte by = 5;
+        short s = 200;
+        int i = 50000;
+        float f = 1.3f;
+        double d = 3.54;
+        char c = 'A';
+        System.out.println(by);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
